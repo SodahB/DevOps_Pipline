e00d75a48ebc69e06091aa9f2798bf0684143510
Sodah Bitar <sodah-@hotmail.com>
2024-11-08 17:36:18 +0100

    kopplat streamlit

--- fetch_weather.py
+++ fetch_weather.py
@@ -1,8 +1,9 @@
 import requests
 from datetime import datetime
-#Liljeholmen statisk location 
+import streamlit as st
+#Stockholm statisk location 
 def get_smhi_data():
-    url = 'https://opendata-download-metfcst.smhi.se/api/category/pmp3g/version/2/geotype/point/lon/18.0215/lat/59.3099/data.json'
+    url = 'https://opendata-download-metfcst.smhi.se/api/category/pmp3g/version/2/geotype/point/lon/18.071094/lat/59.325117/data.json'
     response = requests.get(url)
     return response.json()
 
@@ -10,8 +11,11 @@ def print_temperature_data():
     data = get_smhi_data()
     count = 0  
 
+    st.title ("Temperatur i Stockholm")
+    st.write ("Kommmande 24 timmarna")
+
     for entry in data.get('timeSeries', []):
-        if count >= 10:  #max 10
+        if count >= 24:  #max 24
             break
         
         valid_time = entry.get('validTime')
@@ -24,7 +28,8 @@ def print_temperature_data():
         if temperature is not None:
             #formaterrar valid_time till läsbarare 
             formatted_time = datetime.strptime(valid_time, "%Y-%m-%dT%H:%M:%SZ").strftime("%Y-%m-%d %H:%M")
-            print(f"Tid: {formatted_time}, Temperatur: {temperature}°C")
+            st.write(f"Tid: {formatted_time} Temperatur: {temperature}°C")
             count += 1  
 
-print_temperature_data()
+if __name__ == "__main__":
+    print_temperature_data()
